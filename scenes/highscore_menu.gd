9a5b50d7050ff6aaba7658222363a2fbda3aa04d
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2024-06-25 09:46:32 -0400

    Updated highscore menu looks

--- scenes/highscore_menu.gd
+++ scenes/highscore_menu.gd
@@ -1,15 +1,19 @@
 extends Control
 
+
 @export var leaderboardScene:PackedScene
 var leaderboard
+@onready var margin_container = $MarginContainer
 
 func _on_level_1_button_pressed():
 	create_leaderboard("butter-knights-butter-knights-2wUo")
+	
 
 func _on_level_2_button_pressed():
 	create_leaderboard("butter-knights-level2-ry9g")
 
 func create_leaderboard(leaderboardID):
+	margin_container.hide()
 	leaderboard = leaderboardScene.instantiate()
 	leaderboard.leaderboard_id = leaderboardID
 	add_child(leaderboard)
@@ -25,6 +29,7 @@ func create_leaderboard(leaderboardID):
 
 func _on_button_press():
 	leaderboard.queue_free()
+	margin_container.show()
 
 func _on_main_menu_button_pressed():
 	hide()
