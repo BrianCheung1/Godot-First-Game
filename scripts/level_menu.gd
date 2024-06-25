443d1a08261d357c13429e1245811598af98c1d1
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2024-06-25 18:52:45 -0400

    Added death and reset counters

--- scripts/level_menu.gd
+++ scripts/level_menu.gd
@@ -3,8 +3,14 @@ extends Control
 
 func _on_level_1_button_pressed():
 	print("Level 1")
+	print("test" + get_tree().current_scene.name)
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	PlayerVariables.player_deaths = 0
+	PlayerVariables.player_resets = 0
 
 func _on_level_2_button_pressed():
 	print("Level 2")
+	print("test2" + get_tree().current_scene.name)
 	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+	PlayerVariables.player_deaths = 0
+	PlayerVariables.player_resets = 0
