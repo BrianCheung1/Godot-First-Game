98006d93e9529d4a71bcba8ccc035929a32dfd2f
Jimmy Ma <jiamingma1998@gmail.com>
2024-06-25 17:03:03 -0400

    Explosion death effect'

--- scripts/Monster/Monster2D.gd
+++ scripts/Monster/Monster2D.gd
@@ -1,8 +1,16 @@
 extends Node2D
 class_name Monster2D
 
+const enemy_death_effect = preload("res://scenes/effect_scenes/explosion_4.tscn")
+
 func _init():
 	pass
+	
+func die():
+	self.queue_free()
+	var node = enemy_death_effect.instantiate()
+	get_parent().add_child(node)
+	node.global_position = self.global_position
 	
 func _to_string():
 	return ("Monster")
