d78673a9ea906f7a753bbd4902899d9bd2e75057
DESKTOP-DTHQ2HP\john8 <john82599@gmail.com>
2024-06-29 16:37:56 -0400

    working hotbar

--- scripts/Consumables/Inventory.gd
+++ scripts/Consumables/Inventory.gd
@@ -4,41 +4,63 @@ class_name Inventory
 
 @export var items:Array[Item] = []
 
-#@onready var scene = preload("res://scenes/InventoryGUI.tscn")
-@onready var inventorySlots:Array[VBoxContainer] = [$NinePatchRect/GridContainer/Slot/VBoxContainer, $NinePatchRect/GridContainer/Slot2/VBoxContainer,  $NinePatchRect/GridContainer/Slot3/VBoxContainer,  $NinePatchRect/GridContainer/Slot4/VBoxContainer,  $NinePatchRect/GridContainer/Slot5/VBoxContainer,
-											 $NinePatchRect/GridContainer/Slot6/VBoxContainer, $NinePatchRect/GridContainer/Slot7/VBoxContainer, $NinePatchRect/GridContainer/Slot8/VBoxContainer, $NinePatchRect/GridContainer/Slot9/VBoxContainer, $NinePatchRect/GridContainer/Slot10/VBoxContainer,
-											 $NinePatchRect/GridContainer/Slot11/VBoxContainer, $NinePatchRect/GridContainer/Slot12/VBoxContainer, $NinePatchRect/GridContainer/Slot13/VBoxContainer, $NinePatchRect/GridContainer/Slot14/VBoxContainer, $NinePatchRect/GridContainer/Slot15/VBoxContainer]
-
+@onready var inventory_gui_containers:Array[VBoxContainer] = [$NinePatchRect/GridContainer/Slot/VBoxContainer, $NinePatchRect/GridContainer/Slot2/VBoxContainer,  $NinePatchRect/GridContainer/Slot3/VBoxContainer,  $NinePatchRect/GridContainer/Slot4/VBoxContainer,  $NinePatchRect/GridContainer/Slot5/VBoxContainer,
+													 $NinePatchRect/GridContainer/Slot6/VBoxContainer, $NinePatchRect/GridContainer/Slot7/VBoxContainer, $NinePatchRect/GridContainer/Slot8/VBoxContainer, $NinePatchRect/GridContainer/Slot9/VBoxContainer, $NinePatchRect/GridContainer/Slot10/VBoxContainer,
+													 $NinePatchRect/GridContainer/Slot11/VBoxContainer, $NinePatchRect/GridContainer/Slot12/VBoxContainer, $NinePatchRect/GridContainer/Slot13/VBoxContainer, $NinePatchRect/GridContainer/Slot14/VBoxContainer, $NinePatchRect/GridContainer/Slot15/VBoxContainer]
+@onready var hotbar_gui_containers:Array[VBoxContainer] = [$HotBarGui/NinePatchRect/GridContainer/HotBarSlot1/VBoxContainer, $HotBarGui/NinePatchRect/GridContainer/HotBarSlot2/VBoxContainer,$HotBarGui/NinePatchRect/GridContainer/HotBarSlot3/VBoxContainer,$HotBarGui/NinePatchRect/GridContainer/HotBarSlot4/VBoxContainer,$HotBarGui/NinePatchRect/GridContainer/HotBarSlot5/VBoxContainer]
 @onready var inventory_gui = $"."
+@onready var hidden_store_GUI = $NinePatchRect
 
 const MAX_SLOT = 15
+const MAX_HOT_BAR_INDEX = 4
+
+func _ready():
+	inventory_gui.visible = true
+	hidden_store_GUI.visible = false
 
 func toggle_visibility():
-	inventory_gui.visible = !inventory_gui.visible
+	hidden_store_GUI.visible = !hidden_store_GUI.visible
 
 func _condense_item(item: Item) -> bool:
 	for i in range(self.items.size()):
 		if self.items[i] != null and self.items[i].Name == item.Name:
-			self.items[i].updateQuantity(item.Quantity)
+			self.items[i].update_quantity(item.Quantity)
 			update_sprite_label(str(self.items[i].Quantity),i)
 			return true
 	return false
 
 func update_sprite_label(quantity:String, index:int):
-	var label = self.inventorySlots[index].get_child(1)
+	var label = self.inventory_gui_containers[index].get_child(1)
 	label.text = quantity
+	
+	#This section handles updating the sprite in the hotbar
+	if(index <= MAX_HOT_BAR_INDEX):
+		var hot_box_labal = self.hotbar_gui_containers[index].get_child(1)
+		hot_box_labal.text = quantity
+		
 	return
 	
+
+func swap_index(first_item_index:int, second_item_index:int):
+	return
+
 func update_sprite(texture:Texture, index:int):
-	var sprite = self.inventorySlots[index].get_child(0)
+	#This section handles updating the sprite in the inventory
+	var sprite = self.inventory_gui_containers[index].get_child(0)
 	sprite.texture = texture
 	sprite.scale = Vector2(.15,.15)
+	
+	#This section handles updating the sprite in the hotbar
+	if(index <= MAX_HOT_BAR_INDEX):
+		var hot_box_sprite = self.hotbar_gui_containers[index].get_child(0)
+		hot_box_sprite.texture = texture
+		hot_box_sprite.scale = Vector2(.1,.1)
 	return
 
 func attach_sprite(item:Item, index:int):
 	var texture = load(item.textureSource)
 	if texture:
-		update_sprite(texture, index)
+		update_sprite(texture,index)
 		update_sprite_label(str(item.Quantity), index)
 		
 		return
