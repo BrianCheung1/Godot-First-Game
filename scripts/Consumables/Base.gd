263b7e2875ec76f567a7345c12b551c80e6befa4
DESKTOP-DTHQ2HP\john8 <john82599@gmail.com>
2024-06-29 01:53:34 -0400

    fixed inventory script to be linked to the correct one

--- scripts/Consumables/Base.gd
+++ scripts/Consumables/Base.gd
@@ -5,12 +5,20 @@ var _player: Player
 var _name: String
 var _count: int
 var _level: Node
+var textureSource: String
+const itemSpriteSource = {
+	"Blink Potion" : "res://assets/sprites/1_item.png",
+	"Gravity Potion" : "res://assets/sprites/2_item.png",
+	"Kill All Potion" : "res://assets/sprites/3_item.png",
+	"Suck Coin Potion" : "res://assets/sprites/4_item.png",
+}
 
 func _init(player: Player, name, count):
 	self._player = player
 	self._level = player.level
 	self._name = name
 	self._count = count
+	self.textureSource = itemSpriteSource[name]
 	
 # Subclasses must override this
 func activate():
