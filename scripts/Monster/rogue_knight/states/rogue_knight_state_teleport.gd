6ba6d57989e51a72a110afe8f66f84dd0e4d086c
Jimmy Ma <jiamingma1998@gmail.com>
2024-07-05 18:19:12 -0400

    Fix crash

--- scripts/Monster/rogue_knight/states/rogue_knight_state_teleport.gd
+++ scripts/Monster/rogue_knight/states/rogue_knight_state_teleport.gd
@@ -12,7 +12,7 @@ func enter():
 	logger.print("Entered")
 	rogue_knight = character
 	animated_sprite.play("Teleport")
-	rogue_knight.teleport(Vector2(-78, -103))
+	teleport(Vector2(-78, -103))
 	await animated_sprite.animation_finished
 	on_transition.emit(self, "Idle")
 	
@@ -24,3 +24,9 @@ func update(delta):
 
 func update_physics(delta):
 	pass
+
+func teleport(loc: Vector2):
+	RogueKnightTele.create(rogue_knight, rogue_knight.global_position).go()
+	RogueKnightTele.create(rogue_knight, loc).go()
+	await get_tree().create_timer(0.1).timeout
+	rogue_knight.global_position = loc
